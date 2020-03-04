aa38d3cb9b6dce156142c7ab9158c39eb06636b3
sachinripcord <55999350+sachinripcord@users.noreply.github.com>
2020-03-04 13:52:15 -0800

    Change debug statement

--- s3-copy.py
+++ s3-copy.py
@@ -35,9 +35,10 @@ class S3:
         for root, dirs, files in os.walk(base_path, topdown=False):
             for name in files:
                 source_filename = os.path.join(root, name)
-                print('\nProcessing: {source_filename}'.format(source_filename=source_filename))
 
                 filename = source_filename[len(base_path):]
+                print('\nProcessing: {filename}'.format(filename=filename))
+
                 if not source['suffix'] or filename.endswith(source['suffix']):
                     if not source['prefix'] or filename.startswith(source['prefix']):
                         destination_filename = '{prefix}{source_filename}{suffix}{timestamp}'.format(
